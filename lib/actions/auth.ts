ea90042663ee47c45c746967e4e0644bab7c63b7
Razi Alawamleh <razi.alawamleh@gmail.com>
2025-02-25 13:19:18 -0500

    fixed css issues

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -12,60 +12,58 @@ import { redirect } from "next/navigation";
 // import config from "@/lib/config";
 
 export const signInWithCredentials = async (
-    params: Pick<AuthCredentials, "email" | "password">
+  params: Pick<AuthCredentials, "email" | "password">
 ) => {
-    const { email, password } = params;
+  const { email, password } = params;
 
-    try {
-        const result = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-        });
+  try {
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
 
-        if (result?.error) {
-            return { success: false, error: result.error };
-        }
-    
-        return { success: true };
-    } catch (error) {
-        console.log(error, "Sign in error");
-        return { success: false, error: "" };
-    }
-}
-
-export const signUp = async(params: AuthCredentials) => {
-    const { fullName, email, universityId, universityCard, password } = params;
-
-    //Check if the user already exists
-    const existingUser = await db
-        .select()
-        .from(users)
-        .where(eq(users.email, email))
-        .limit(1);
-
-    if(existingUser.length > 0) {
-        return { success: false, error: "User already exists" };
+    if (result?.error) {
+      return { success: false, error: result.error };
     }
 
-    const hashedPassword = await hash(password, 10);
+    return { success: true };
+  } catch (error) {
+    console.log(error, "Sign in error");
+    return { success: false, error: "" };
+  }
+};
 
-    try {
-        await db.insert(users).values({
-            fullName,
-            email,
-            universityId,
-            universityCard,
-            password: hashedPassword,
-        })
+export const signUp = async (params: AuthCredentials) => {
+  const { fullName, email, universityId, universityCard, password } = params;
 
-        await signInWithCredentials({ email, password });
+  //Check if the user already exists
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
 
-        return { success: true };
+  if (existingUser.length > 0) {
+    return { success: false, error: "User already exists" };
+  }
 
-    } catch (error) {
-        console.log(error , "Sign up error");
-        return { success: false, error: "" };
+  const hashedPassword = await hash(password, 10);
 
-    }
+  try {
+    await db.insert(users).values({
+      fullName,
+      email,
+      universityId,
+      universityCard,
+      password: hashedPassword,
+    });
+
+    await signInWithCredentials({ email, password });
+
+    return { success: true };
+  } catch (error) {
+    console.log(error, "Sign up error");
+    return { success: false, error: "" };
+  }
 };
